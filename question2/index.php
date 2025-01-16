2979ee04b0b68e8db498843ab4de6833779d6b1a
Croquignolex <geptitalex10@gmail.com>
2025-01-16 13:20:26 -0500

    First commit

---
+++ question2/index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Question 1</title>
+    </head>
+
+    <body>
+        ...
+        <!-- Le reste du contenu -->
+        ...
+    </body>
+</html>
